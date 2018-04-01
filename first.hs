ac04b63a32acb47c96d5f91013ecf22a5da1b6f6
Dmitry <130ndim@gmail.com>
2018-04-01 18:38:39 +0300

    Delete first.hs

--- first.hs
+++
@@ -1,8 +0,0 @@
-sqrt_n :: Double -> Double -> Double
-sqrt_n m n | (abs (n * n - m) < 0.1) = n
-           | otherwise = sqrt_n m ((n+m/n)/2)
-           
-main :: IO()
-main = do
-      x <- readLn
-      print (round(sqrt_n x 1))
